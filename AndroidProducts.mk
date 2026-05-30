ec27345bce086d3fecf6d3e2f2e84e19200fa0a9
tbengu <tbengu@bhengubv.com>
2026-05-30 03:39:52 +0200

    AndroidProducts.mk: migrate COMMON_LUNCH_CHOICES to 3-part naming
    Same migration as the redmi_note12 sibling repo: AOSP 15 lunch combos
    must be <product>-<release>-<variant>. The old 2-part form is rejected
    by modern lunch.
    
    Renaming to:
      circle_pixel6-trunk_staging-userdebug
      circle_pixel6-trunk_staging-user

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,7 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/device.mk
 
+# AOSP 15 lunch combos are 3-part: <product>-<release>-<variant>.
 COMMON_LUNCH_CHOICES := \
-    circle_pixel6-userdebug \
-    circle_pixel6-user
+    circle_pixel6-trunk_staging-userdebug \
+    circle_pixel6-trunk_staging-user
